aa7662100ae5cc3df7a5120ab5ef04d7b12d4aff
Felipe101070 <felipej.araujo@gmail.com>
2023-10-13 00:47:40 -0300

    teste

--- src/main/java/br/com/felipearaujo/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/felipearaujo/todolist/task/ITaskRepository.java
@@ -5,5 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-    List<TaskModel> findByIdUser (UUID idUser);
+    List<TaskModel> findByIdUser (UUID idUser); 
 }
